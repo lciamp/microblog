7481a3ee4df83f13f8d2c808cfcf238a5c5dcbc4
Lou <lou@Lous-iMac.home>
2018-06-25 19:32:55 -0400

    added markdown for posts

--- app/main/forms.py
+++ app/main/forms.py
@@ -3,6 +3,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField, BooleanField, SelectField, ValidationError
 from wtforms.validators import DataRequired, Length, Email, Regexp
+from flask_pagedown.fields import PageDownField
 from ..models import Role, User
 
 
@@ -41,5 +42,5 @@ class EditProfileAdminForm(FlaskForm):
             raise ValidationError('Username already registered.')
 
 class PostForm(FlaskForm):
-    body = TextAreaField("What's on your mind?", validators=[DataRequired()])
+    body = PageDownField("What's on your mind?", validators=[DataRequired()])
     submit = SubmitField('Submit Post')
