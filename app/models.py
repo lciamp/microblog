fafac94ed547221d1f339be3097059417cd687b3
Lou <lou@Lous-iMac.home>
2018-06-26 03:13:16 -0400

    added social features

--- app/models.py
+++ app/models.py
@@ -72,6 +72,14 @@ class Role(db.Model):
         return '<Role %r>' % self.name
 
 
+class Follow(db.Model):
+    __tablename__ = 'follows'
+
+    follower_id = db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True)
+    followed_id = db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True)
+    timestamp   = db.Column(db.DateTime, default=datetime.utcnow)
+
+
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
 
@@ -87,8 +95,20 @@ class User(UserMixin, db.Model):
     member_since    = db.Column(db.DateTime(), default=datetime.utcnow)
     last_seen       = db.Column(db.DateTime(), default=datetime.utcnow)
     avatar_hash     = db.Column(db.String(32))
-    posts           = db.relationship('Post', backref='author', lazy='dynamic')
 
+    posts           = db.relationship('Post',
+                                      backref='author',
+                                      lazy='dynamic')
+    followed        = db.relationship('Follow',
+                                      foreign_keys=[Follow.follower_id],
+                                      backref=db.backref('follower', lazy='joined'),
+                                      lazy='dynamic',
+                                      cascade='all, delete-orphan')
+    followers        = db.relationship('Follow',
+                                      foreign_keys=[Follow.followed_id],
+                                      backref=db.backref('followed', lazy='joined'),
+                                      lazy='dynamic',
+                                      cascade='all, delete-orphan')
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -190,6 +210,26 @@ class User(UserMixin, db.Model):
         return '{url}/{hash}?s={size}&d={default}&r={rating}'.format(
             url=url, hash=hash, size=size, default=default, rating=rating)
 
+    def follow(self, user):
+        if not self.is_following(user):
+            f = Follow(follower=self, followed=user)
+            db.session.add(f)
+
+    def unfollow(self, user):
+        f = self.followed.filter_by(followed_id=user.id).first()
+        if f:
+            db.session.delete(f)
+
+    def is_following(self, user):
+        if user.id is None:
+            return False
+        return self.followed.filter_by(followed_id=user.id).first() is not None
+
+    def is_followed_by(self, user):
+        if user.id is None:
+            return False
+        return self.followers.filter_by(follower_id=user.id).first() is not None
+
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -229,3 +269,31 @@ class Post(db.Model):
 
 
 db.event.listen(Post.body, 'set', Post.on_changed_body)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
