faafde1e73f1644bfc5609476733f631204429fa
Lou <lou@Lous-iMac.home>
2018-07-10 21:58:07 -0400

    cleanup

--- bootstrap.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export FLASK_APP=flasky.py && export FLASK_DEBUG=1
-export FLASKY_ADMIN='louis.ciampanelli@gmail.com'
-export SECRET_KEY='zkRnTGmcD2cGV0jTDEB5ycsjD7XQ+z'
-echo "boostrap done"
